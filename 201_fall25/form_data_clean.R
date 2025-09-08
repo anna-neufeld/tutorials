e1f5cafa5b85a01cc9baa800a4dac6680435a4b3
anna-neufeld <acn2@williams.edu>
2025-09-08 16:14:32 -0400

    added year

--- 201_fall25/form_data_clean.R
+++ 201_fall25/form_data_clean.R
@@ -1,9 +1,12 @@
 form_data <- read.csv("~/Dropbox/WilliamsTeaching/tutorials/201_fall25/form_data.csv")
-form_data <- form_data[-c(1,6),]
-form_data_anonymous <- form_data[,c(17:29)]
+form_data <- form_data[-c(6,14),]
+form_data_anonymous <- form_data[,c(9, 17:29)]
 
-names(form_data_anonymous) <- c("pets", "sleep_semester", "sleep_school", "cups.coffee", "height", "sports",
+names(form_data_anonymous) <- c("year", "pets", "sleep_semester", "sleep_school", "cups.coffee", "height", "sports",
                                 "music", "favorite.coffee", "favorite.dining", "campus.summer", "div.three", "labs",
                                 "route2")
 
 write.csv(form_data_anonymous, file="form_data_anonymous.csv", row.names = F)
+
+
+form_data <- form_data_anonymous
