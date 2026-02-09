55a765f27aea5ce11a5d13cd92995bfdabc883a6
anna-neufeld <acn2@williams.edu>
2026-02-09 14:29:03 -0500

    name fix

--- 201_spring25/form_data_clean.R
+++ 201_spring25/form_data_clean.R
@@ -1,13 +1,12 @@
 form_data <- read.csv("~/Dropbox/WilliamsTeaching/tutorials/201_spring25/form_data.csv")
 form_data <- form_data[-c(6,14),]
-form_data_anonymous <- form_data[,c(9, 17:29)]
+form_data_anonymous <- form_data[,c(9, 20:31)]
 
 names(form_data_anonymous) <- c("year", "pets", "sleep.semester", "sleep.summer", "cups.coffee", "height", "sports",
-                                "music", "favorite.coffee", "favorite.dining", "campus.summer", "div.three", "labs",
+                                "music", "favorite.dining", "campus.winterstudy", "div.three", "labs",
                                 "route2")
 
-Stat 201_ Getting to know you survey (Spring) (Responses) - Form Responses 1 (1).csv
+setwd("~/Dropbox/WilliamsTeaching/tutorials/201_spring25")
 write.csv(form_data_anonymous, file="form_data_anonymous.csv", row.names = F)
-
 
 form_data <- form_data_anonymous
